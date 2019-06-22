6c32231ad361881638ea500eb69b81dcad6ab9a0
Jordan Hennessy <jorrdygirl@me.com>
2019-06-22 12:29:42 -0500

    Exercise 8.3

---
+++ week-8/hennessy_calculator.py
@@ -0,0 +1,28 @@
+# Title:  hennessy_calculator.py
+# Author: Professor Krasso
+# Date:   22 June 2019
+# Modified By: Jordan Hennessy
+# Description: Python in Action
+
+first_name = 'Jordan'
+last_name = 'Hennessy'
+print(first_name + ' ' + last_name)
+
+
+
+def add(x,y):
+  return x + y
+
+
+def subtract(x,y):
+  return x - y
+
+
+def divide(x,y):
+  return x / y
+
+
+
+print(add(1,2))
+print(subtract(4,1))
+print(divide(8,2))
