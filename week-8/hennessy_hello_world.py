ab416bd96c354946f0befe1cca74ae55893bec18
Jordan Hennessy <jorrdygirl@me.com>
2019-06-22 11:47:47 -0500

    Exercise 8.2

---
+++ week-8/hennessy_hello_world.py
@@ -0,0 +1,11 @@
+/*
+============================================
+; Title:  hennessy_hello_world.py
+; Author: Professor Krasso
+; Date:   22 June 2019
+; Modified By: Jordan Hennessy
+; Description: Python Hello World
+;===========================================
+*/
+
+print('Hello World')
